469e4d40f581f793175e47f81a3e1cc9e68315f1
Mohamed EL-Khouly <msirag95@gmail.com>
2021-04-06 12:47:08 +0200

    added create all method

--- database/models.py
+++ database/models.py
@@ -34,6 +34,18 @@ def setup_db(app, database_path=database_path):
     migrate.init_app(app, db)
 
 
+'''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+'''
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+
 # ----------------------------------------------------------------------------#
 # Models.
 # ----------------------------------------------------------------------------#
